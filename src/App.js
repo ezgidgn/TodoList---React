41d2f3cf25cc034084ba57daec74bd12e252eff4
burakmetinn <burakmetin579@gmail.com>
2023-07-04 15:07:40 +0300

    Create Todo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import React , {useState , useEffect} from "react"
-import {AiOutlinePlus} from 'react-icons/ai';
+import React, { useState, useEffect } from "react"
+import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from "./Todo";
+import {db} from "./firebase"
+import  {query, collection, onSnapshot, updateDoc, doc, addDoc} from "firebase/firestore"
 
 const style = {
-  bg:'h-screen w-screen p-4 bg-gradient-to-r from-[#d9d7ce] to-[#453a06]',
-  container:"bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4",
-  heading:"text-3xl font-bold text-center text-gray-800 p-2",
-  form:"flex justify-between", 
-  input:"border p-2 w-full text-xl",
-  button:"border p-4 ml-2 bg-gray-500 text-slate-100",
-  count:"text-center p-2"
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-[#d9d7ce] to-[#453a06]',
+  container: "bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4",
+  heading: "text-3xl font-bold text-center text-gray-800 p-2",
+  form: "flex justify-between",
+  input: "border p-2 w-full text-xl",
+  button: "border p-4 ml-2 bg-gray-500 text-slate-100",
+  count: "text-center p-2"
 }
 
 function App() {
-  const [todos,setTodos] = useState(["Learn React", "Grind Leetcode"])
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+  
+
+  //Create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e)
+    if(input === ""){
+      alert("Please enter a todo")
+      return
+    }
+    await addDoc(collection(db,"todos"),{
+      text: input,
+      completed: false,
+    }) 
+    setInput("")
+  }
 
   return (
 
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form}>
-          <input className={style.input} type="text" placeholder="Add Todo"></input>
-          <button className={style.button}><AiOutlinePlus size={30}/></button>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type="text"
+            placeholder="Add Todo"></input>
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
         </form>
         <ul>
-          {todos.map((todo , index)=> (
-              <Todo key={index} todo={todo}/>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
           ))}
         </ul>
-        <p className={style.count}>You have 2 todos</p>
+        <p className={style.count}>You have {todos.length} todos</p>
       </div>
     </div>
   );
