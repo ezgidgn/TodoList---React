2153ea87ef2f267e1f7da4b1cdb2549b1d39dc2c
burakmetinn <burakmetin579@gmail.com>
2023-07-04 13:42:21 +0300

    Css update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React , {useState , useEffect} from "react"
 import {AiOutlinePlus} from 'react-icons/ai';
+import Todo from "./Todo";
 
 const style = {
-  bg:'h-screen w-screen p-4 bg-gradient-to-r from- [#2F80ED] to-[#1CB5E0]'
+  bg:'h-screen w-screen p-4 bg-gradient-to-r from-[#d9d7ce] to-[#453a06]',
+  container:"bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4",
+  heading:"text-3xl font-bold text-center text-gray-800 p-2",
+  form:"flex justify-between", 
+  input:"border p-2 w-full text-xl",
+  button:"border p-4 ml-2 bg-gray-500 text-slate-100",
+  count:"text-center p-2"
 }
 
 function App() {
+  const [todos,setTodos] = useState(["Learn React", "Grind Leetcode"])
 
   return (
 
@@ -17,8 +25,11 @@ function App() {
           <button className={style.button}><AiOutlinePlus size={30}/></button>
         </form>
         <ul>
-          
+          {todos.map((todo , index)=> (
+              <Todo key={index} todo={todo}/>
+          ))}
         </ul>
+        <p className={style.count}>You have 2 todos</p>
       </div>
     </div>
   );
