2391b46501e2af11dac1681473ebc61019f3c0e8
Omar Abdelhamed <omar792001@gmail.com>
2023-07-12 02:11:09 +0300

    done its ready

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -3,9 +3,11 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
 import { db } from '../firebase';
 import { BsCalendarCheck } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
 import image from '../assets/images/aaron-burden-xG8IQMqMITM-unsplash.jpg';
 import {
   query,
+  where,
   collection,
   onSnapshot,
   updateDoc,
@@ -14,37 +16,73 @@ import {
   deleteDoc,
 } from 'firebase/firestore';
 import AnimatedPage from './AnimatedPage';
+import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router';
 
 function Crud() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
 
   //Create todo
   const createTodo = async (e) => {
-    e.preventDefault(e);
+    e.preventDefault();
     if (input === '') {
       alert('Please enter a todo');
       return;
     }
-    await addDoc(collection(db, 'todos'), {
+
+    const auth = getAuth();
+    const currentUser = auth.currentUser;
+    const userId = currentUser ? currentUser.uid : null;
+
+    const todo = {
       text: input,
       completed: false,
-    });
+      userId: userId,
+    };
+
+    await addDoc(collection(db, 'todos'), todo);
     setInput('');
   };
 
   //Read to do from firebase
   useEffect(() => {
-    const q = query(collection(db, 'todos'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const auth = getAuth();
+
+    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      if (unsubscribeAuth) {
+        unsubscribeAuth();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const q = query(collection(db, 'todos'), where('userId', '==', user.uid));
+    const unsubscribeSnapshot = onSnapshot(q, (querySnapshot) => {
       let todosArr = [];
       querySnapshot.forEach((doc) => {
         todosArr.push({ ...doc.data(), id: doc.id });
       });
       setTodos(todosArr);
     });
-    return () => unsubscribe();
-  }, []);
+
+    return () => {
+      if (unsubscribeSnapshot) {
+        unsubscribeSnapshot();
+      }
+    };
+  }, [user]);
+
+
 
   //Update todo in in firebase
   const toggleComplete = async (todo) => {
@@ -56,6 +94,21 @@ function Crud() {
   //Delete todo in in firebase
   const deleteTodo = async (todoID) => {
     await deleteDoc(doc(db, 'todos', todoID));
+  };
+
+  //logout
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Logout successful
+        console.log('User logged out successfully');
+        navigate('/');
+      })
+      .catch((error) => {
+        // An error occurred during logout
+        console.log('Error occurred during logout:', error);
+      });
   };
 
   return (
@@ -74,11 +127,17 @@ function Crud() {
         </div>
       </AnimatedPage>
 
-      <div className='bg-slate-100 max-w-[700px] w-full m-auto rounded-md shadow-xl p-4  absolute left-[27vw] top-[27vh] backdrop-blur-sm bg-white/30'>
+      <div className='bg-slate-100 max-w-[700px] w-full m-auto rounded-md shadow-xl p-4  absolute left-[27vw] top-[27vh] backdrop-blur-sm bg-white/30 shadow-xl'>
         <AnimatedPage>
           <h3 className=' text-3xl mb-5 font-bold text-center text-white top-[50px]'>
             What are you Doing Today?
           </h3>
+          <button
+            onClick={handleLogout}
+            className=' float-right absolute right-4 top-4 flex '
+          >
+            <BiLogOut id='logout' className='text-3xl hover:scale-125 ' />
+          </button>
           <form onSubmit={createTodo} className='flex justify-between '>
             <input
               value={input}
